6725e8cdd78f259dfdabfecd9637f3d9668cf2eb
wkwyatt <wkwyatt1@gmail.com>
2015-11-12 15:08:08 -0500

    added marker to the google map with map api

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -4,15 +4,33 @@ myApp.controller('mapCtrl', function($scope) {
 	var mapOption = {
 		zoom: 4,
 			// center of the united states
-		center: new google.maps.LatLng(40.0000, -98.0000), 
-		mapTypeId: google.maps.mapTypeId.TERRAIN
+		center: new google.maps.LatLng(40.0000, -98.0000)
 	}
 
-	$scope.map = new google.maps.Map(document.getElementById('map'),)
+	$scope.map = new google.maps.Map(document.getElementById('map'), mapOption);
 	$scope.markers = [];
 
-	var infoWindow = new google.maps.infoWindow();
+	var infoWindow = new google.maps.InfoWindow();
+	var createMarker = function(city, index) {
+		console.log(city);
+		console.log(index);
+		console.log(typeof(latLon));
 
+		var latLon = city.latLon.split(',');
+		var lat = latLon[0];
+		var lon = latLon[1];
+
+		var marker = new google.maps.Marker({
+			map: $scope.map,
+			position: new google.maps.LatLng(lat, lon),
+			title: city.city,
+			icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2%7CFE7569'
+		});
+
+
+	}
 	$scope.cities = cities;
-	
+    for(i=0;i<cities.length;i++){
+        createMarker(cities[i],i)
+    }
 });
