597d7f4c8d93659d92c4c0128051ba5be2d16f80
oliverlai55 <oliverlai55@gmail.com>
2015-11-12 17:57:03 -0500

    add function lodgingSearch

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -82,12 +82,36 @@ myApp.controller('mapCtrl', function($scope) {
 
 	}
 
-	$scope.lodgingSearch = function(city) {
+	$scope.lodgingSearch = function(latLon) {
 		map = new google.maps.Map(document.getElementById('map'), {
-	    center: pyrmont,
+	    center: latLon,
 	    zoom: 15
 	  });
-	}
+
+		infowindow = new google.maps.InfoWindow();
+
+  		var service = new google.maps.places.PlacesService(map);
+  		service.nearbySearch({
+  			location: latLon,
+  			radius: 500,
+  			types: ['lodging']
+  		}, callback);
+  	}
+  	function callback(results, status) {
+  		if (status === google.maps.places.PlacesServiceStatus.OK) {
+  			for (var i = 0; i < results.length; i++) {
+  				createMarker(results[i]);
+  			}
+  		}
+  	}
+
+
+  	google.maps.event.addListener(marker, 'click', function() {
+  		infowindow.setContent(place.name);
+  		infowindow.open(map, this);
+  	});
+  }
+	
 
 	getDirections = function (lat, lon) {
 		// 
