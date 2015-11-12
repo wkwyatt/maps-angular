6aa6373abb6ca3607bd9a0d1121bba1c6cc75523
wkwyatt <wkwyatt1@gmail.com>
2015-11-12 13:54:39 -0500

    added map object to mapCtrl

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,4 +1,10 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('mapCtrl', function($scope) {
-	
+
+	var mapOption = {
+		zoom: 4,
+			// center of the united states
+		center: new google.maps.LatLng(40.0000, -98.0000), 
+		mapTypeId: google.maps.mapTypeId.TERRAIN
+	}
 });
