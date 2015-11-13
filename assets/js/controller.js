8d5ac2ddd4d392de1128160df625ab7ed5f8aeda
turboflanagan <turboflanagan@gmail.com>
2015-11-13 13:23:08 -0500

    delete conflicted function

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -62,7 +62,6 @@ myApp.controller('mapCtrl', function($scope) {
 		google.maps.event.trigger($scope.markers[i-1], 'click');
 	}
 
-<<<<<<< HEAD
 	$scope.updateMarkers = function() {
 		//for loop clears all markers when it runs.
 		for(i=0;i < $scope.markers.length; i++) {
@@ -117,24 +116,6 @@ myApp.controller('mapCtrl', function($scope) {
   		});
   	}
 
-	
-
-||||||| merged common ancestors
-	$scope.updateMarkers = function() {
-		alert("test");
-		for(i=0;i < $scope.markers.length; i++) {
-			$scope.markers[i].setMap(null);
-		}
-
-		for (var i = 0; i < $scope.filteredCities.length; i++) {
-			createMarker($scope.filteredCities[i], i);
-		};
-
-		console.log($scope.filteredCities);
-	}
-
-=======
->>>>>>> master
 	getDirections = function (lat, lon) {
 		// 
 		var directionsService = new google.maps.DirectionsService();
